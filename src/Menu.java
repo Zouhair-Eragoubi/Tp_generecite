1cac62cdffd13f9d16dd64a3f05bd80d8ef7cd29
ZouhairEragoubi <zouhair@asic.ma>
2023-01-14 23:11:56 +0100

    first commit

--- src/Menu.java
+++ src/Menu.java
@@ -16,14 +16,16 @@ public class Menu {
 		System.out.println("3 -- Ajouter produit");
 		System.out.println("4 -- Supprimer produit par ID");
 		System.out.println("5 -- Enregistrer tout");
-		System.out.println("2 -- Quitter");
+		System.out.println("6 -- Quitter");
 		while(sys) {
+			System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * ");
 			System.out.println("Veuillez saisir un nombre :");
 			int str = sc.nextInt();
 			switch (str) {
 			case 1:
+				
 				System.out.println("List des produit");
-				for (Produit produit : listProduit.getAll()) {
+				for (Produit produit : listProduit.getListProduits()) {
 					System.out.println(produit.toString());
 					System.out.println("*************************");
 				}
@@ -39,19 +41,33 @@ public class Menu {
 				}
 				break;
 			case 3:
+				
 				System.out.println("-- Ajouter produit --");
 				Produit prod = new Produit();
-				System.out.println("-- Donner id produit --");
-				prod.setId(sc.nextLong());
-				System.out.println("-- Donner Marque --");
-				prod.setMarque(sc.next());
-				System.out.println("-- Donner prix --");
-				prod.setPrix(sc.nextFloat());
-				System.out.println("-- Donner dsecription --");
-				prod.setDescription(sc.next());
-				System.out.println("-- Donner qte en stock --");
-				prod.setQteStock(sc.nextLong());
-				listProduit.add(prod);
+				boolean sys2 = true;
+				while(sys2) {
+					System.out.println("-- Donner id produit --");
+					Long id = sc.nextLong();
+					if(listProduit.findById(id) == null) {
+						prod.setId(id);
+						System.out.println("-- Donner nom --");
+						prod.setNom(sc.next());
+						sc.nextLine();
+						System.out.println("-- Donner Marque --");
+						prod.setMarque(sc.next());
+						System.out.println("-- Donner prix --");
+						prod.setPrix(sc.nextFloat());
+						System.out.println("-- Donner dsecription --");
+						prod.setDescription(sc.next());
+						System.out.println("-- Donner qte en stock --");
+						prod.setQteStock(sc.nextLong());
+						listProduit.add(prod);
+						sys2 = false;
+					}else {
+						System.out.println("-- id produit existe deja --");
+					}
+				}
+				
 				break;
 			case 4:
 				System.out.println("3-- Supprimer par ID");
